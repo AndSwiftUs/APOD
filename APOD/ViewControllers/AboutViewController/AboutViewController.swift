507b7bb74c31fb045b5431779474c7c335b8de3b
Andrew Ustintchik <AndSwiftUs@gmail.com>
2022-07-21 23:18:17 +0300

    fixed facing layout problems

--- APOD/ViewControllers/AboutViewController/AboutViewController.swift
+++ APOD/ViewControllers/AboutViewController/AboutViewController.swift
@@ -12,11 +12,22 @@ class AboutViewController: UIViewController {
         
         addChild(hosingFile)
         
-        hosingFile.view.frame = self.view.frame
+        hosingFile.view.frame = self.view.bounds
         
         self.view.addSubview(hosingFile.view)
-        
+        view.addConstrained(subview: hosingFile.view)
         hosingFile.didMove(toParent: self)
         
+    }
+}
+
+extension UIView {
+    func addConstrained(subview: UIView) {
+        addSubview(subview)
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        subview.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        subview.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        subview.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        subview.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
     }
 }
